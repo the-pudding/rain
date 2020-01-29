c3ffcd5693d939641154f2c22bac700a1a024e66
Amber Thomas <ProQuestionAsker@gmail.com>
2020-01-29 09:41:29 -0800

    Adding bars

--- web/src/js/annual.js
+++ web/src/js/annual.js
@@ -20,125 +20,125 @@ let readerCity = null;
 let stepIDs = null;
 
 function setupStepIDs() {
-    stepIDs = [
-        {
-            step: 0,
-            ids: ['USW00094290', 'USW00012815'],
-        },
-        {
-            step: 1,
-            ids: ['USW00094290', 'USW00012815', readerStation],
-        },
-    ];
+  stepIDs = [
+    {
+      step: 0,
+      ids: ['USW00094290', 'USW00012815'],
+    },
+    {
+      step: 1,
+      ids: ['USW00094290', 'USW00012815', readerStation],
+    },
+  ];
 }
 
 function handleStepEnter(response) {
-    const { index } = response;
-    console.log({ index });
+  const { index } = response;
+  console.log({ index });
 }
 
 function setupScroll() {
-    scroller
-        .setup({
-            step: '.annual .step',
-            offset: 0.7,
-            debug: true,
-        })
-        .onStepEnter(handleStepEnter);
+  scroller
+    .setup({
+      step: '.annual .step',
+      offset: 0.7,
+      debug: true,
+    })
+    .onStepEnter(handleStepEnter);
 }
 
 function calculatingDistance(readerLat, readerLong, locLat, locLong) {
-    // Haversine Formula
-    function toRadians(value) {
-        return (value * Math.PI) / 180;
-    }
+  // Haversine Formula
+  function toRadians(value) {
+    return (value * Math.PI) / 180;
+  }
 
-    const R = 3958.756; // miles
-    const φ1 = toRadians(readerLat);
-    const φ2 = toRadians(locLat);
-    const Δφ = toRadians(locLat - readerLat);
-    const Δλ = toRadians(locLong - readerLong);
+  const R = 3958.756; // miles
+  const φ1 = toRadians(readerLat);
+  const φ2 = toRadians(locLat);
+  const Δφ = toRadians(locLat - readerLat);
+  const Δλ = toRadians(locLong - readerLong);
 
-    const a =
-        Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-        Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const a =
+    Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+    Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-    return R * c;
+  return R * c;
 }
 
 function findNearestStation() {
-    locationDistance = data
-        .map(d => ({
-            ...d,
-            distance: calculatingDistance(
-                readerLatLong.latitude,
-                readerLatLong.longitude,
-                +d.latitude,
-                +d.longitude
-            ),
-        }))
-        .filter(d => !isNaN(d.distance));
+  locationDistance = data
+    .map(d => ({
+      ...d,
+      distance: calculatingDistance(
+        readerLatLong.latitude,
+        readerLatLong.longitude,
+        +d.latitude,
+        +d.longitude
+      ),
+    }))
+    .filter(d => !isNaN(d.distance));
 
-    locationDistance.sort((a, b) => d3.descending(a.distance, b.distance));
-    const location = locationDistance.pop();
-    readerStation = location.id;
-    readerCity = location.city;
+  locationDistance.sort((a, b) => d3.descending(a.distance, b.distance));
+  const location = locationDistance.pop();
+  readerStation = location.id;
+  readerCity = location.city;
 }
 
 function filterData(step) {
-    return data.filter(d => stepIDs[step].ids.includes(d.id));
+  return data.filter(d => stepIDs[step].ids.includes(d.id));
 }
 
 function setupChart() {
-    const filtered = filterData(0);
+  const filtered = filterData(0);
 
-    const chart = $container.datum(filtered).puddingBar();
-    console.log({ filtered });
+  const chart = $container.datum(filtered).puddingBar();
+  chart.resize().render();
 }
 
 function setup() {
-    findNearestStation();
-    setupStepIDs();
-    setupChart();
-    setupScroll();
+  findNearestStation();
+  setupStepIDs();
+  setupChart();
+  setupScroll();
 
-    resize();
+  resize();
 
-    console.log({ stepIDs });
+  console.log({ stepIDs });
 }
 
 function cleanData(data) {
-    const clean = data.map(d => ({
-        ...d,
-        latitude: +d.latitude,
-        longitude: +d.longitude,
-        average: +d.average,
-        rank: +d.rank,
-        total19: +d.total19,
-        elevation: +d.elevation,
-    }));
+  const clean = data.map(d => ({
+    ...d,
+    latitude: +d.latitude,
+    longitude: +d.longitude,
+    average: +d.average,
+    rank: +d.rank,
+    total19: +d.total19,
+    elevation: +d.elevation,
+  }));
 
-    return clean;
+  return clean;
 }
 
 function resize() {
-    // 1. update height of step elements
-    const stepH = Math.floor(window.innerHeight * 0.75);
+  // 1. update height of step elements
+  const stepH = Math.floor(window.innerHeight * 0.75);
 
-    $steps.style('height', `${stepH}px`);
+  $steps.style('height', `${stepH}px`);
 
-    scroller.resize();
+  scroller.resize();
 }
 
 function init(readerLocation) {
-    loadData('annual_precip.csv')
-        .then(result => {
-            data = cleanData(result);
-            readerLatLong = readerLocation;
-            setup();
-        })
-        .catch(console.error);
+  loadData('annual_precip.csv')
+    .then(result => {
+      data = cleanData(result);
+      readerLatLong = readerLocation;
+      setup();
+    })
+    .catch(console.error);
 }
 
 export default { init, resize };
--- web/src/js/pudding-chart/annual-template.js
+++ web/src/js/pudding-chart/annual-template.js
@@ -19,20 +19,26 @@ d3.selection.prototype.puddingBar = function init(options) {
 
     // data
     let data = $chart.datum();
+    console.log({ data });
 
     // dimensions
     let width = 0;
     let height = 0;
     const MARGIN_TOP = 0;
     const MARGIN_BOTTOM = 0;
-    const MARGIN_LEFT = 0;
-    const MARGIN_RIGHT = 0;
+    const MARGIN_LEFT = 16;
+    const MARGIN_RIGHT = 16;
+    const BAR_HEIGHT = 40;
+    const PADDING = 0.1;
+    const LOC_PADDING = 100;
+    const FONT_SIZE = 16;
 
     // scales
-    const scaleX = null;
-    const scaleY = null;
+    const scaleX = d3.scaleLinear();
+    const scaleY = d3.scaleBand();
 
     // helper functions
+    const formatThousands = d3.format(',d');
 
     const Chart = {
       // called once at start
@@ -53,12 +59,66 @@ d3.selection.prototype.puddingBar = function init(options) {
         $svg
           .attr('width', width + MARGIN_LEFT + MARGIN_RIGHT)
           .attr('height', height + MARGIN_TOP + MARGIN_BOTTOM);
+        const totals = data.map(d => d.total19);
+        const max = d3.max(totals);
+        scaleX.domain([0, max]).range([0, width - LOC_PADDING]);
+        scaleY
+          .domain(d3.range(0, 11))
+          .range([0, height])
+          .padding(PADDING);
+
         return Chart;
       },
       // update scales and render chart
       render() {
         // offset chart for margins
         $vis.attr('transform', `translate(${MARGIN_LEFT}, ${MARGIN_TOP})`);
+        console.log('running');
+
+        const $groups = $vis
+          .selectAll('.g-location')
+          .data(data)
+          .join(enter => enter.append('g').attr('class', 'g-location'))
+          .attr('transform', (d, i) => `translate(0, ${scaleY(i)})`);
+
+        console.log({ $groups });
+        $groups
+          .selectAll('.bar')
+          .data(d => [d])
+          .join(enter =>
+            enter.append('rect').attr('class', d => `bar bar-${d.id}`)
+          )
+          .attr('x', LOC_PADDING)
+          .attr('y', 0)
+          .attr('width', d => scaleX(d.total19))
+          .attr('height', BAR_HEIGHT);
+
+        $groups
+          .selectAll('.location')
+          .data(d => [d])
+          .join(enter => enter.append('text').attr('class', 'location'))
+          .text(d => d.city)
+          .attr('alignment-baseline', 'middle')
+          .attr(
+            'transform',
+            `translate(${LOC_PADDING - MARGIN_LEFT}, ${BAR_HEIGHT / 2})`
+          )
+          .attr('text-anchor', 'end');
+
+        $groups
+          .selectAll('.annual__amount')
+          .data(d => [d])
+          .join(enter => enter.append('text').attr('class', 'annual__amount'))
+          .text(d => `${formatThousands(d.total19)} mm`)
+          .attr('alignment-baseline', 'middle')
+          .attr(
+            'transform',
+            d =>
+              `translate(${scaleX(d.total19) +
+              LOC_PADDING -
+              MARGIN_RIGHT}, ${BAR_HEIGHT / 2})`
+          )
+          .attr('text-anchor', 'end');
 
         return Chart;
       },
