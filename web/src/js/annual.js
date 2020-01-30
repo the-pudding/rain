0b3dbe7e0dbfffe0e8b69c5f1d2ae2919dd93a38
Amber Thomas <ProQuestionAsker@gmail.com>
2020-01-29 18:13:26 -0800

    adding line breaks to bars

--- web/src/js/annual.js
+++ web/src/js/annual.js
@@ -10,6 +10,7 @@ const $section = d3.select('.annual');
 const $container = $section.select('figure');
 const $svg = $container.select('svg');
 const $steps = $section.selectAll('.step');
+const $titleLoc = d3.select('.readerCity')
 
 // constants
 let data = null;
@@ -26,188 +27,189 @@ let rankMap = null;
 let index = 0;
 
 function setupStepIDs() {
-  stepIDs = [
-    {
-      step: 0,
-      ids: [SEATTLE, ORLANDO],
-    },
-    {
-      step: 1,
-      ids: [SEATTLE, ORLANDO],
-    },
-    {
-      step: 2,
-      ids: [SEATTLE],
-    },
-    {
-      step: 3,
-      ids: [SEATTLE],
-    },
-  ];
+    stepIDs = [
+        {
+            step: 0,
+            ids: [SEATTLE, ORLANDO],
+        },
+        {
+            step: 1,
+            ids: [SEATTLE, ORLANDO],
+        },
+        {
+            step: 2,
+            ids: [SEATTLE],
+        },
+        {
+            step: 3,
+            ids: [SEATTLE],
+        },
+    ];
 }
 
 function calculatingDistance(readerLat, readerLong, locLat, locLong) {
-  // Haversine Formula
-  function toRadians(value) {
-    return (value * Math.PI) / 180;
-  }
+    // Haversine Formula
+    function toRadians(value) {
+        return (value * Math.PI) / 180;
+    }
 
-  const R = 3958.756; // miles
-  const φ1 = toRadians(readerLat);
-  const φ2 = toRadians(locLat);
-  const Δφ = toRadians(locLat - readerLat);
-  const Δλ = toRadians(locLong - readerLong);
+    const R = 3958.756; // miles
+    const φ1 = toRadians(readerLat);
+    const φ2 = toRadians(locLat);
+    const Δφ = toRadians(locLat - readerLat);
+    const Δλ = toRadians(locLong - readerLong);
 
-  const a =
-    Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-    Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const a =
+        Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+        Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-  return R * c;
+    return R * c;
 }
 
 function findNearestStation() {
-  locationDistance = data
-    .map(d => ({
-      ...d,
-      distance: calculatingDistance(
-        readerLatLong.latitude,
-        readerLatLong.longitude,
-        +d.latitude,
-        +d.longitude
-      ),
-      seattle: calculatingDistance(
-        readerLatLong.latitude,
-        readerLatLong.longitude,
-        47.6872,
-        -122.2553
-      ),
-    }))
-    .filter(d => !isNaN(d.distance));
+    locationDistance = data
+        .map(d => ({
+            ...d,
+            distance: calculatingDistance(
+                readerLatLong.latitude,
+                readerLatLong.longitude,
+                +d.latitude,
+                +d.longitude
+            ),
+            seattle: calculatingDistance(
+                readerLatLong.latitude,
+                readerLatLong.longitude,
+                47.6872,
+                -122.2553
+            ),
+        }))
+        .filter(d => !isNaN(d.distance));
 
-  locationDistance.sort((a, b) => d3.descending(a.distance, b.distance));
-  const location = locationDistance.pop();
-  // is the reader within 50 miles of Seattle?
-  inSeattle = location.seattle <= 50;
-  console.log({ inSeattle, sea: location.seattle });
-  // if within 50 miles of Seattle, compare to NYC, otherwise, use reader's location
-  readerStation = inSeattle ? 'USW00094728' : location.id;
-  readerCity = location.city;
-  // add the reader Station ID to the filtered data for steps 2 & 3
-  stepIDs[1].ids.push(readerStation);
-  stepIDs[2].ids.push(readerStation);
-  stepIDs[3].ids.push(readerStation);
+    locationDistance.sort((a, b) => d3.descending(a.distance, b.distance));
+    const location = locationDistance.pop();
+    // is the reader within 50 miles of Seattle?
+    inSeattle = location.seattle <= 50;
+    console.log({ inSeattle, sea: location.seattle });
+    // if within 50 miles of Seattle, compare to NYC, otherwise, use reader's location
+    readerStation = inSeattle ? 'USW00094728' : location.id;
+    readerCity = inSeattle ? 'New York City' : location.city;
+    $titleLoc.text(readerCity)
+    // add the reader Station ID to the filtered data for steps 2 & 3
+    stepIDs[1].ids.push(readerStation);
+    stepIDs[2].ids.push(readerStation);
+    stepIDs[3].ids.push(readerStation);
 }
 
 function setupRankMap(index) {
-  let ranks = null;
-  if (index === 3) {
-    ranks = data
-      .sort((a, b) => d3.descending(a.average, b.average))
-      .map((d, i) => [d.id, i]);
-  } else {
-    ranks = data
-      .sort((a, b) => d3.descending(a.total19, b.total19))
-      .map((d, i) => [d.id, i]);
-  }
+    let ranks = null;
+    if (index === 3) {
+        ranks = data
+            .sort((a, b) => d3.descending(a.average, b.average))
+            .map((d, i) => [d.id, i]);
+    } else {
+        ranks = data
+            .sort((a, b) => d3.descending(a.total19, b.total19))
+            .map((d, i) => [d.id, i]);
+    }
 
-  rankMap = new Map(ranks);
+    rankMap = new Map(ranks);
 }
 
 function findWettest() {
-  const wettest19 = data
-    // .map(d => ({ ...d }))
-    .sort((a, b) => d3.descending(a.total19, b.total19));
-  const top1019 = wettest19.slice(0, 10).map(d => d.id);
-  top1019.forEach(d => stepIDs[2].ids.push(d));
-  console.log({ top1019 });
+    const wettest19 = data
+        // .map(d => ({ ...d }))
+        .sort((a, b) => d3.descending(a.total19, b.total19));
+    const top1019 = wettest19.slice(0, 10).map(d => d.id);
+    top1019.forEach(d => stepIDs[2].ids.push(d));
+    console.log({ top1019 });
 
-  const wettestOverall = data
-    .map(d => ({ ...d }))
-    .sort((a, b) => d3.descending(a.average, b.average));
-  const top10Overall = wettestOverall.slice(0, 10).map(d => d.id);
-  top10Overall.forEach(d => stepIDs[3].ids.push(d));
-  console.log({ top1019, top10Overall, stepIDs });
+    const wettestOverall = data
+        .map(d => ({ ...d }))
+        .sort((a, b) => d3.descending(a.average, b.average));
+    const top10Overall = wettestOverall.slice(0, 10).map(d => d.id);
+    top10Overall.forEach(d => stepIDs[3].ids.push(d));
+    console.log({ top1019, top10Overall, stepIDs });
 }
 
 function filterData(step) {
-  const filtered = data.filter(d => stepIDs[step].ids.includes(d.id));
-  if (step === 3) filtered.sort((a, b) => d3.descending(a.average, b.average));
-  else filtered.sort((a, b) => d3.descending(a.total19, b.total19));
-  return filtered;
+    const filtered = data.filter(d => stepIDs[step].ids.includes(d.id));
+    if (step === 3) filtered.sort((a, b) => d3.descending(a.average, b.average));
+    else filtered.sort((a, b) => d3.descending(a.total19, b.total19));
+    return filtered;
 }
 
 function handleStepEnter(response) {
-  index = response.index;
-  const filtered = filterData(index);
-  setupRankMap(index);
-  console.log({ filtered });
-  chart
-    .data(filtered)
-    .resize(index)
-    .render({ index, rankMap });
+    index = response.index;
+    const filtered = filterData(index);
+    setupRankMap(index);
+    console.log({ filtered });
+    chart
+        .data(filtered)
+        .resize(index)
+        .render({ index, rankMap });
 }
 
 function setupScroll() {
-  scroller
-    .setup({
-      step: '.annual .step',
-      offset: 0.7,
-      debug: true,
-    })
-    .onStepEnter(handleStepEnter);
+    scroller
+        .setup({
+            step: '.annual .step',
+            offset: 0.7,
+            debug: false,
+        })
+        .onStepEnter(handleStepEnter);
 }
 
 function setupChart() {
-  const filtered = filterData(0);
+    const filtered = filterData(0);
 
-  chart = $container.datum(filtered).puddingBar();
-  chart.resize(0).render({ index: 0, rankMap });
+    chart = $container.datum(filtered).puddingBar();
+    chart.resize(0).render({ index: 0, rankMap });
 }
 
 function setup() {
-  setupStepIDs();
-  findNearestStation();
-  findWettest();
-  setupRankMap();
-  setupChart();
-  setupScroll();
+    setupStepIDs();
+    findNearestStation();
+    findWettest();
+    setupRankMap();
+    setupChart();
+    setupScroll();
 
-  resize();
+    resize();
 }
 
 function cleanData(data) {
-  const clean = data.map(d => ({
-    ...d,
-    latitude: +d.latitude,
-    longitude: +d.longitude,
-    average: +d.average,
-    rank: +d.rank,
-    total19: +d.total19,
-    elevation: +d.elevation,
-  }));
+    const clean = data.map(d => ({
+        ...d,
+        latitude: +d.latitude,
+        longitude: +d.longitude,
+        average: +d.average,
+        rank: +d.rank,
+        total19: +d.total19,
+        elevation: +d.elevation,
+    }));
 
-  return clean;
+    return clean;
 }
 
 function resize() {
-  // 1. update height of step elements
-  const stepH = Math.floor(window.innerHeight * 0.75);
+    // 1. update height of step elements
+    const stepH = Math.floor(window.innerHeight * 0.75);
 
-  $steps.style('height', `${stepH}px`);
+    $steps.style('height', `${stepH}px`);
 
-  scroller.resize();
-  chart.resize(index).render({ index, rankMap });
+    scroller.resize();
+    chart.resize(index).render({ index, rankMap });
 }
 
 function init(readerLocation) {
-  loadData('annual_precip.csv')
-    .then(result => {
-      data = cleanData(result);
-      readerLatLong = readerLocation;
-      setup();
-    })
-    .catch(console.error);
+    loadData('annual_precip.csv')
+        .then(result => {
+            data = cleanData(result);
+            readerLatLong = readerLocation;
+            setup();
+        })
+        .catch(console.error);
 }
 
 export default { init, resize };
--- web/src/js/pudding-chart/annual-template.js
+++ web/src/js/pudding-chart/annual-template.js
@@ -28,8 +28,8 @@ d3.selection.prototype.puddingBar = function init(options) {
     const MARGIN_BOTTOM = 0;
     const MARGIN_LEFT = 16;
     const MARGIN_RIGHT = 16;
-    const BAR_HEIGHT = 40;
-    const PADDING = 0.1;
+    let BAR_HEIGHT = 20;
+    const PADDING = 0.2;
     const LOC_PADDING = 150;
     const FONT_SIZE = 16;
     const TRANSITION_SPEED = 500;
@@ -69,15 +69,16 @@ d3.selection.prototype.puddingBar = function init(options) {
           .range([0, height])
           .padding(PADDING);
 
+        BAR_HEIGHT = height / 12 - PADDING * 12;
+
         return Chart;
       },
       // update scales and render chart
       render({ index, rankMap }) {
-        console.log({ index, rankMap });
         // offset chart for margins
         $vis.attr('transform', `translate(${MARGIN_LEFT}, ${MARGIN_TOP})`);
-        console.log('running');
 
+        // add groups and move them to correct y location
         const $groups = $vis
           .selectAll('.g-location')
           .data(data, d => d.id)
@@ -95,6 +96,7 @@ d3.selection.prototype.puddingBar = function init(options) {
                 .attr('transform', (d, i) => `translate(0, ${scaleY(i)})`)
           );
 
+        // add bars to each group
         $groups
           .selectAll('.bar')
           .data(d => [d])
@@ -111,6 +113,7 @@ d3.selection.prototype.puddingBar = function init(options) {
             index === 3 ? scaleX(d.average) : scaleX(d.total19)
           );
 
+        // add location titles to each group
         $groups
           .selectAll('.location')
           .data(d => [d])
@@ -127,6 +130,7 @@ d3.selection.prototype.puddingBar = function init(options) {
           )
           .attr('text-anchor', 'end');
 
+        // add amount annotation to each group
         $groups
           .selectAll('.annual__amount')
           .data(d => [d])
@@ -147,6 +151,37 @@ d3.selection.prototype.puddingBar = function init(options) {
                   MARGIN_RIGHT}, ${BAR_HEIGHT / 2})`
           )
           .attr('text-anchor', 'end');
+        let checkTopBar = null;
+        let checkBottomBar = null;
+
+        if (index >= 2) {
+          console.log({ check: rankMap.get(data[10].id) });
+          const firstAdd = rankMap.get(data[10].id) + 1;
+          const secondAdd = rankMap.get(data[11].id) + 1;
+
+          checkTopBar = firstAdd === 11;
+          checkBottomBar = secondAdd === 12 || secondAdd === firstAdd + 1;
+        }
+
+        console.log({ index });
+        // add dotted lines for broken graphic
+        $vis
+          .selectAll('.break')
+          .data([0, 1])
+          .join(enter =>
+            enter.append('line').attr('class', (d, i) => `break break-${i}`)
+          )
+          .attr('y1', d => (d === 0 ? scaleY(10) : scaleY(11)))
+          .attr('y2', d => (d === 0 ? scaleY(10) : scaleY(11)))
+          .attr('x1', 0)
+          .attr('x2', width)
+          .classed(
+            'is-hidden',
+            d =>
+              index < 2 ||
+              (d === 0 && checkTopBar) ||
+              (d === 1 && checkBottomBar)
+          );
 
         return Chart;
       },
