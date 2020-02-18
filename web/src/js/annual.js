791ed748f709b70fb199206114cd54de6ef4197e
Amber Thomas <ProQuestionAsker@gmail.com>
2020-02-18 12:36:59 -0800

    update dev version

--- web/src/js/annual.js
+++ web/src/js/annual.js
@@ -138,7 +138,7 @@ function setupScroll() {
     .setup({
       step: '.annual .step',
       offset: scrollOffset,
-      debug: false,
+      debug: true,
     })
     .onStepEnter(handleStepEnter);
 }
